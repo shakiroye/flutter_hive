efea106ef1240146e12c59e19d8f06716df5081d
shakiroye <oyeossiadewoye@gmail.com>
2022-09-21 00:47:35 +0000

    finished ! :)

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,25 +9,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // reference our box
+  final _myBox = Hive.box('mybox');
+  // write date
+  void writeData() {
+    _myBox.put(2, 'Mary');
+  }
+
+  // read data
+  void readData() {
+    print(_myBox.get(2));
+  }
+
+  //delete date
+  void deleteData() {
+    _myBox.delete(1);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             MaterialButton(
-              onPressed: () {},
+              onPressed: writeData,
               child: Text('Write'),
               color: Colors.blue[200],
             ),
             MaterialButton(
-              onPressed: () {},
-              child: Text('Write'),
+              onPressed: readData,
+              child: Text('Read'),
               color: Colors.blue[200],
             ),
             MaterialButton(
-              onPressed: () {},
-              child: Text('Write'),
+              onPressed: deleteData,
+              child: Text('Delete'),
               color: Colors.blue[200],
             ),
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hive/home_page.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-void main() async{
-
+void main() async {
   //intialise hive
   await Hive.initFlutter();
 
   // Open the box
-  var box= await Hive.openBox('mybox');
+  var box = await Hive.openBox('mybox');
 
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const HomePage();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      // home: const ScanEvents(),
+    );
   }
 }
